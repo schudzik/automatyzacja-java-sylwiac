7097bd66414e7bc7fa56d5facc70bef02e6f9a5d
Sylwia.Chudzik <KiniaXenia1>
2018-12-10 22:16:06 +0100

    a

---
+++ src/main/java/Zadanie1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+
+        String wzorzec = "exit";
+        String zKlawiatury = "";
+
+        System.out.println("Witaj świecie");
+        while (!(zKlawiatury.equals(wzorzec))) {
+
+            System.out.println("Aby zamknac aplikację wpisz exit i wciśnij klawisz enter");
+            Scanner odczyt = new Scanner(System.in);
+            zKlawiatury = odczyt.nextLine();
+
+        }
+    }
+}
+
+
+
+
+
