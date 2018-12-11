1e650a01023faaddcdb99f88409774029448baaf
schudzik <schudzik@poczta.fm>
2018-12-11 12:23:47 +0100

    a

--- src/main/java/kalkulator/KalkulatorApp.java
+++ src/main/java/kalkulator/KalkulatorApp.java
@@ -6,9 +6,14 @@ public class KalkulatorApp {
 
         return x + y;
     }
-    public double add(double x, double y){
-        return x+y;
+    public double add(double x, double y) {
+        return x + y;
     }
 
+
+
+        public double div(double x, double y) {
+            return x / y;
+        }
 
 }
--- src/test/java/kalkulator/KalkulatorAppTest.java
+++ src/test/java/kalkulator/KalkulatorAppTest.java
@@ -5,30 +5,45 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-@DisplayName("Kocurek")
 
+@DisplayName("Kocurek")
 class KalkulatorAppTest {
 
     @Test
     public void addTest() {
         //arrange
         KalkulatorApp kalkulatorek = new KalkulatorApp();
-                //act
-       int suma = kalkulatorek.add(1,2);
+        //act
+
+
+        int suma = kalkulatorek.add(6, 2);
 
         //assert
-        Assertions.assertEquals(3,suma, "Sprawdź,że dodoawanie liczb całkowitych działa");
+        Assertions.assertEquals(3, suma, "Sprawdź,że dodoawanie liczb całkowitych działa");
     }
 
     @Test
+
     public void nowy() {
 
-    KalkulatorApp kalkulatorek = new KalkulatorApp();
-    //act
+        KalkulatorApp kalkulatorek = new KalkulatorApp();
+        //act
 
-    double suma = kalkulatorek.add(1.3,1.7);
+        double suma = kalkulatorek.add(1.3, 1.7);
 
-    //assert
-        Assertions.assertEquals(3,suma, "Sprawdź,że dodoawanie liczb całkowitych działa");
+        //assert
+        Assertions.assertEquals(3, suma, "Sprawdź,że dodoawanie liczb całkowitych działa");
     }
+
+    @Test
+
+    public void dzielenie() {
+
+        KalkulatorApp kalkulatorek = new KalkulatorApp();
+        //act
+
+        double dzielenie = kalkulatorek.div(6.0, 2.0);
+
+        //assert
+        Assertions.assertEquals(3, dzielenie, "Sprawdź,że");}
 }
